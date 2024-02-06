f7d284a2d0076169d078a9f3ec3e770bcd65ff6a
Joshua Epstein <joshsepstein@gmail.com>
2024-02-06 19:49:23 +0000

    Add files via upload

--- Java/Industry Simulator/prettier.config.js
+++ Java/Industry Simulator/prettier.config.js
@@ -1,1 +1,13 @@
-modules.export = {}
+modules.export = {
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    printWidth: 100,
+    tabWidth: 4,
+    semi: true,
+    useTabs: false,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    endOfLine: 'lf',
+}
